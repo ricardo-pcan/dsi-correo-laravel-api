07cca2b2409fcdb10d98eb6356a1bdd8c98da36f
Ricardo <ricardo.pcan@gmail.com>
2015-12-01 20:05:37 -0600

    Add namespace for api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,33 +10,36 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::resource('users', 'UserController');
+Route::group(['namespace' => 'api'], function()
+{
+    Route::resource('users', 'UserController');
 
-Route::get( 'dsi_users', [
-    'uses' => 'UserController@get_dsi_users',
-   'as' => 'get_dsi_users'
-]);
+    Route::get( 'dsi_users', [
+        'uses' => 'UserController@get_dsi_users',
+        'as' => 'get_dsi_users'
+    ]);
 
-Route::get( 'admins', [
-    'uses' => 'UserController@get_admins',
-    'as' => 'get_admins'
-]);
+    Route::get( 'admins', [
+        'uses' => 'UserController@get_admins',
+        'as' => 'get_admins'
+    ]);
 
-Route::resource('requests', 'RequestController',
-    [ 'only' => [ 'index' ] ] );
+    Route::resource('requests', 'RequestController',
+        [ 'only' => [ 'index' ] ] );
 
-Route::resource('users.requests', 'UserRequestController',
-    [ 'except' => [ 'create' ] ] );
+    Route::resource('users.requests', 'UserRequestController',
+        [ 'except' => [ 'create' ] ] );
 
-Route::resource( 'requests.users', 'RequestUserController',
-    [ 'only' => [ 'index', 'destroy' ] ] );
+    Route::resource( 'requests.users', 'RequestUserController',
+        [ 'only' => [ 'index', 'destroy' ] ] );
 
-Route::resource('users.comments', 'UserCommentController',
-    [ 'except' => [ 'destroy' ] ]);
+    Route::resource('users.comments', 'UserCommentController',
+        [ 'except' => [ 'destroy' ] ]);
 
-Route::resource( 'requests.comments', 'RequestCommentController',
-    [ 'only' => [ 'index', 'destroy' ] ]);
+    Route::resource( 'requests.comments', 'RequestCommentController',
+        [ 'only' => [ 'index', 'destroy' ] ]);
+});
 
 Route::post('oauth/access_token', function() {
-    return Response::json(Authorizer::issueAccessToken());
-});
+        return Response::json(Authorizer::issueAccessToken());
+    });
